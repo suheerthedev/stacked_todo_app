23310489f541b7acfadee8fec2db8a2360b0d3f8
Suheer Khan <khansuheer1@gmail.com>
2024-12-19 10:44:37 +0500

    Wrote fetch data functionality in ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,8 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_todo_app/app/app.locator.dart';
+
 class ApiService {
+  final DialogService _dialogService = locator<DialogService>();
+  Future<void> fetchData() async {
+    final url =
+        Uri.parse('https://softcodix.pythonanywhere.com/api/serviceprovider/');
 
-  Future<void> fetchData() async{
-    const url = 'https://softcodix.pythonanywhere.com/api/serviceprovider/';
+    try {
+      final response = await http.get(url);
 
-    
+      if (response.statusCode == 200) {
+        final responseBody = jsonDecode(response.body);
+        print(responseBody);
+      } else {
+        _dialogService.showDialog(
+            title: "Failed to load data :(",
+            description: response.statusCode.toString());
+      }
+    } catch (error) {
+      _dialogService.showDialog(
+          title: "Failed to fetch data", description: "Error: $error");
+    }
   }
 }
